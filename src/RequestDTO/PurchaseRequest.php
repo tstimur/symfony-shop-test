eb22c963bbca8c853ccc2e4b0c6eb9355893f43b
Timur <cymzhitov.t@divan.ru>
2025-10-17 20:39:59 +0700

    feat: Add PriceCalculationRequest and PurchaseRequest DTOs

---
+++ src/RequestDTO/PurchaseRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\RequestDTO;
+
+use App\Validator\ValidTaxNumber;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class PurchaseRequest
+{
+    #[Assert\NotBlank(message: 'Product ID is required')]
+    #[Assert\Type('integer', message: 'Product ID must be an integer')]
+    #[Assert\Positive(message: 'Product ID must be positive')]
+    public ?int $product = null;
+
+    #[Assert\NotBlank(message: 'Tax number is required')]
+    #[Assert\Type('string', message: 'Tax number must be a string')]
+    #[ValidTaxNumber]
+    public ?string $taxNumber = null;
+
+    #[Assert\Type('string', message: 'Coupon code must be a string')]
+    public ?string $couponCode = null;
+
+    #[Assert\NotBlank(message: 'Payment processor is required')]
+    #[Assert\Type('string', message: 'Payment processor must be a string')]
+    #[Assert\Choice(
+        choices: ['paypal', 'stripe'],
+        message: 'Payment processor must be either paypal or stripe'
+    )]
+    public ?string $paymentProcessor = null;
+}

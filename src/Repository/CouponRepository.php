48144f948fc90665dfd7b99739a89d03bd37f214
Timur <cymzhitov.t@divan.ru>
2025-10-17 21:05:59 +0700

    fix: remove comments

--- src/Repository/CouponRepository.php
+++ src/Repository/CouponRepository.php
@@ -17,29 +17,4 @@ class CouponRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Coupon::class);
     }
-
-    //    /**
-    //     * @return Coupon[] Returns an array of Coupon objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Coupon
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }

833ba5b584bf44998c3e9ffb70661fc635583d65
Timur <cymzhitov.t@divan.ru>
2025-10-20 00:23:53 +0700

    feat: Add price calculation and purchase API endpoints

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Exception\PaymentProcessorException;
 use App\Request\DTO\PriceCalculationRequestDTO;
 use App\Request\DTO\PurchaseRequestDTO;
 use App\Service\PaymentProcessorRegistry;
 use App\Service\PriceCalculatorService;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -26,13 +30,47 @@ final class PaymentController extends AbstractController
     #[Route('/calculate-price', methods: ['POST'])]
     public function calculatePrice(PriceCalculationRequestDTO $dto): JsonResponse
     {
+        $priceData = $this
+            ->priceCalculatorService
+            ->calculatePrice(
+                $dto->product,
+                $dto->taxNumber,
+                $dto->couponCode
+            );
 
-        return $this->json([]);
+        return $this->json([
+            'message' => 'Price calculated',
+            'price' => $priceData
+        ],
+            Response::HTTP_OK
+        );
     }
 
+    /**
+     * @throws PaymentProcessorException
+     * @throws Exception
+     */
     #[Route('/purchase', methods: ['POST'])]
     public function purchase(PurchaseRequestDTO $dto): JsonResponse
     {
-        return $this->json([]);
+        $priceData = $this->priceCalculatorService->calculatePrice(
+            $dto->product,
+            $dto->taxNumber,
+            $dto->couponCode,
+        );
+
+        $processor = $this
+            ->paymentProcessorRegistry
+            ->get($dto->paymentProcessor);
+
+        $processor->process($priceData['total']);
+
+        return $this->json(
+            [
+                'message' => 'Purchase successful',
+                'total' => $priceData['total'],
+                ],
+            Response::HTTP_OK
+        );
     }
 }
